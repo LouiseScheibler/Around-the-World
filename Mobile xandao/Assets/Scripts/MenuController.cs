4009a4251a3b44895f7d930a0e60a16a81f2bc1f
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-03-30 21:29:34 -0300

    Esquema de UI do Menu completo
    Só falta as artes e escrever o que quiser.
    
    GameController -> Responsável pelo botão de voltar para o menu;
    MenuController -> Responsável pelos botões do menu.
    
    Quaisquer duvidas, só perguntar.

---
+++ Mobile xandao/Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject creditsPainel, controlsPainel;
+
+    public void PlayBTN()
+    {
+        SceneManager.LoadScene("scene");
+    }
+
+    public void OpenCreditsPainel()
+    {
+        creditsPainel.SetActive(true);
+    }
+    public void CloseCreditsPainel()
+    {
+        creditsPainel.SetActive(false);
+    }
+    public void OpenControlsPainel()
+    {
+        controlsPainel.SetActive(true);
+    }
+    public void CloseControlsPainel()
+    {
+        controlsPainel.SetActive(false);
+    }
+
+    public void QuitBTN()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+}
