be751f88b24d903acbd181dbcca7323bf2ec9b17
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-05-05 10:04:20 -0300

    "Concluido" o pedido

--- Mobile xandao/Assets/Scripts/Scripts quebra cabeça/PerguntasBTNConttoler.cs
+++ Mobile xandao/Assets/Scripts/Scripts quebra cabeça/PerguntasBTNConttoler.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PerguntasBTNConttoler : MonoBehaviour
 {
-
+    public static PerguntasBTNConttoler Instance;
     public string nextLevel;
     public GameObject congratsPainel;
+    public List<GameObject> wrongBtnList = new List<GameObject>();
+    public string actualScene;
 
+    private void Start()
+    {
+        Instance = this;
+    }
+    private void Update()
+    {
+        if(wrongBtnList.Count >= 2)
+        {
+            SceneManager.LoadScene(actualScene);
+        }
+        
+    }
     public void BTNCerto()
     {
         if (SceneManager.GetActiveScene().buildIndex > PlayerPrefs.GetInt("faseCompleta"))
@@ -23,7 +40,11 @@ public class PerguntasBTNConttoler : MonoBehaviour
 
     public void BTNErrado()
     {
-
+        if (!WrongBTN.instance.isDisable)
+        {
+            WrongBTN.instance.isDisable = true;
+            wrongBtnList.Add(WrongBTN.instance.button);
+        }
     }
     public void BTNLastQuestionCorect()
     {
